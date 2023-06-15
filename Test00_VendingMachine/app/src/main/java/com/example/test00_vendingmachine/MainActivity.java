4f6df39dcc6b89b53c508ad5c5f88b356cdc7d41
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-06-15 10:52:52 +0900

    ++

---
+++ Test00_VendingMachine/app/src/main/java/com/example/test00_vendingmachine/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.test00_vendingmachine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button add_btn;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        add_btn = findViewById(R.id.add_btn);
+
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, CheckPassword.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
